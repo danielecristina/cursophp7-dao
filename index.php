201ace22215323caacb44cce0ed38cf882235e89
Daniele Cristina <dani.lu.mila@hotmail.com>
2018-12-29 13:28:13 -0200

    metodos de lista

--- index.php
+++ index.php
@@ -7,10 +7,25 @@ $sql = new Sql();
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 echo json_encode($usuarios);*/
 
+/*Carrega um usuário
 $root = new Usuario();
-
 $root->loadById(3);
+echo $root; */
 
-echo $root;
+/*Carrega uma lista de usuários, como getList() é um método estatico não precisa ser instanciado
+$lista = Usuario::getList();
+echo json_encode($lista);*/
+
+/* Carrega uma lista de usuários buscando pelo login
+$search = Usuario::search("o");
+echo json_encode($search);*/
+
+// Carrega usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("percy", "anaklusmos");
+
+echo $usuario;
+
+// https://www.youtube.com/watch?v=aF5IjX4zvRs&index=13&list=PLVOaFkNst_AAmvDFVls5DhCnvzvlHd6Fn
 
 ?>
